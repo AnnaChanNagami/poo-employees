ec646cbb658ac8e9bc662b11c53f9bc6a27850be
Amine FODIL-CHERIF <elodie.linsac@gmail.com>
2024-06-25 08:38:18 +0200

    java poo +algo tri

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -1,4 +1,5 @@
 package fr.afpa.employees;
+import java.time.LocalDate;
 
 /*
 Objectif :
@@ -8,32 +9,145 @@ permettant de gérer (vraiment très basiquement) des salariés d’une entrepri
 Vous allez écrire une classe représentant les salariés d'une entreprise.
 Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
 */
-
-class Employee
-{
+// on est passé de 30 à 15 erreurs c'est déjà ça :'(
+class Employee {
 	/**
-	 * Matricule de l'employé
+	 * Matricule de l'employé + attributs
 	 */
 	private String registrationNumber;
+	private String lastName;
+	private String firstName;
+	// ici on ne met pas int on met double car on peut avoir une valeur à virgule
+	private double saLary;
+	private static final int socialParticipation = 30;
+	// ici on met l'attribut pour la date d'anniversaire
+	private LocalDate birthDay;
 
-	// TODO compléter les attributs comme présenté dans le PDF
-
-	// TODO compléter le constructeur de la classe
-	public Employee(String registrationNumber) {
+	// comment implémenter les données de chacun ? on doit faire reginum1 par
+	// exemple ?
+	// compléter le constructeur de la classe (ça m'a l'air bon)
+	// constructeur de la classe
+	public Employee(String registrationNumber, String lastName, String firstName, double saLary) {
 		this.registrationNumber = registrationNumber;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.saLary = saLary;
+		//  la classe localDate nous permet de manipuler des dates 
+		this.birthDay = LocalDate.parse(birthDay);
 	}
 
-	// TODO implémenter les setters et getters de la classe (permet d'accéder aux attributs privés)
+	// compléter les attributs comme présenté dans le PDF done je crois
+	// est-ce qu'ici le code sait à qui appartient les informations ?
+	// // Employee employee1 = new Employee("123456", "Jebot", "Thomas", "1800",
+	// "0.3");
+	// Employee employee2 = new Employee("654321", "Tobej", "Samoht", "3000",
+	// "0.3");
+	// Employee employee3 = new Employee("153426", "Komta", "Karine", "2600",
+	// "0.3");
+	// Employee employee4 = new Employee("426153", "Atmok", "Enirak", "3300",
+	// "0.3");
+
+	// implémenter les setters et getters de la classe permet d'accéder aux
+	// attributs privés (m'a l'air bon ? )
+
+	// 1) Getters :
+
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
 
-	public void setRegistrationNumber(String registrationNumber) throws Exception {
+	public String getLastName() {
+		return lastName;
+	}
+
+	public double getSaLary() {
+		return saLary;
+	}
+
+	public int getSocialParticipation1() {
+		return socialParticipation;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public LocalDate getBirthDay() {
+		return birthDay;
+	}
+
+	// 2) Setters :
+	public void setRegistrationNumber(String registrationNumber) {
+		// throws Exception
 		this.registrationNumber = registrationNumber;
 	}
 
-	// TODO ajouter les getters et les setters
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui représente un objet de la classe employé
-	// plus d'information sur la méthode "toString()" -> https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	public void setSaLary(int saLary) {
+		this.saLary = saLary;
+	}
+
+	public void setSocialParticipation(int socialParticipation) {
+		this.socialParticipation = socialParticipation;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setBirthDay(String birthDay) throws Exception {
+		if (!checkDateFormat(birthDay)) {
+			throw new Exception("Le format de la date est incorrect. Utilisez le format AAAA-MM-JJ.");
+		}
+		this.birthDay = LocalDate.parse(birthDay);
+	}
+
+	// ajouter les getters et les setters
+
+	// public Employee(String registrationNumber, String lastName, String firstName,
+	// int saLary) {
+	// this.registrationNumber = registrationNumber;
+	// this.lastName = lastName;
+	// this.firstName = firstName;
+	// this.saLary = saLary;
+	// }
+
+	@Override
+	public String toString() {
+
+		return "Employee { "
+				+ " \nMatricule " + getRegistrationNumber()
+				// + "\nverifie le matricule "+ checkRegistrationNumber(registrationNumber)
+				+ " \nNom " + getLastName()
+				+ " \nPrenom " + getFirstName()
+				+ " \nSalaire " + getSalary()
+				// + " \nDate d'anniversaire " + getBirthDay()
+				// + "\nSalaire net " + netSalary()
+				// + "\nJours avant prochain anniversaire " + daysBeforeBirthday()
+				+ '}';
+	}
+
+	// méthode pour uniquement le nom + prénom ?
+	public Employee(String lastName, String firstName) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		System.out.println(lastName + firstName);
+	}
+
+	// méthode pour obtenir le salaire net du salaire brut
+	public double toNetsalary() {
+		return this.saLary - this.saLary * (socialParticipation / 100);
+	}
 }
+// public String toString(){
+// return "Employee [registrationNumber=" + registrationNumber + ", lastName=" +
+// lastName +", firstName=" + firstName +", saLaray=" + salary +",
+// socialParticipation=" + socialParticipation +"]";
+
+// implémenter la méthode "toString()" qui renvoie une chaîne de caractère
+// qui représente un objet de la classe employé
+// plus d'information sur la méthode "toString()" ->
+// https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -12,10 +12,17 @@ class EmployeeMain
 	{
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
-		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22");
-
-		// TODO afficher les informations des employés avec System.out.println
+		//  instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
+		Employee employee1 = new Employee("123456", "Jebot", "Thomas", "1800", "0.3");
+		Employee employee2 = new Employee("654321", "Tobej", "Samoht", "3000", "0.3");
+		Employee employee3 = new Employee("153426", "Komta", "Karine", "2600", "0.3");
+		Employee employee4 = new Employee("426153", "Atmok", "Enirak", "3300", "0.3");
+	
+		//  afficher les informations des employés avec System.out.println
+		System.out.println("voilà les données du premier employé" + employee1 );
+	    System.out.println("voilà les données du deuxième employé" + employee2 );
+	    System.out.println("voilà les données du troisième employé" + employee3 );
+	    System.out.println("voilà les données du quatrième employé"+ employee4 );
 	}
 }
 
